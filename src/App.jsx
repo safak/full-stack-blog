eee0d5248a06b6432d187fc1c7a10d8028199040
Nazar <my@gmail.com>
2024-12-05 23:14:47 -0500

    added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import NavBar from "./components/NavBar"
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className='px-4 md:px-8 lg:px-16 xl:px-3'>
+      <NavBar />
+      {/* <BreadCrumb /> */}
+      {/* <Introduction /> */}
+      {/* <Featured post /> */}
+      {/* <post list /> */}
+
+    </div>
   )
 }
 
