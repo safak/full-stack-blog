d82068b23db76ac9bb4a3d748365e07a63a6921f
Sanskruti825 <borhadesanskruti@gmail.com>
2024-12-11 13:17:33 +0530

    SearchPage Layout  Draft 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import Navbar from "./components/Navbar"
-
+import { SearchBar } from './components/SearchBar';
+import Card  from './components/Card.jsx';
 const App = () => {
   return (
     <div className=''>
+      <div className="search-bar-container"></div>
       <Navbar/>
+      <SearchBar/>
+      <div className="card-container">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      </div>
+
     </div>
   )
 }
