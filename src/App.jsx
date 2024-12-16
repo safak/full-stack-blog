a6d058fabe9c3e50b7278543fd9a1668cb4792ce
Fajar Abdurrohman <sepertiseharusnyafajar@gmail.com>
2024-12-16 19:48:24 +0700

    add-navbar-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className='px-4 md:px-8 lg:px-16 lx:px-32 3xl:px-64'>Hello World</div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+const Navbar = () => {
+    return (
+        <div className="">
+
+            {/* LOGO */}
+            <div className="">
+                <img src="/logo.png" className="w-8 h-8" alt="" />
+                <span>fajarblog</span>
+            </div>
+
+            {/* MOBILE MENU */}
+            <div className="">
+
+            </div>
+
+            {/* DESKTOP MENU */}
+            <div className="hidden md:flex">
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
+
