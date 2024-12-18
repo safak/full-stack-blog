c7bc44442cb347fd6bc6fbafcd2a024935508aa1
Khansa Ibrahimi <khansa08a@gmail.com>
2024-12-18 03:35:08 -0500

    cleaned App.jsx file for merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import Categories from "./components/Categories"
-import Features from "./components/Features"
-import HeroSection from "./components/HeroSection"
-import Navbar from "./components/Navbar"
-import Review from "./components/Review"
-import Footer from "./components/Footer"
+
 import "./index.css" 
 
 const App = () => {
