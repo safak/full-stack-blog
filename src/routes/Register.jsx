9329fe068672f08a0f4856d57f2bd22ac037ee1b
Khansa Ibrahimi <khansa08a@gmail.com>
2024-12-21 02:28:55 -0500

    commit before merging with serach-page

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,12 +1,6 @@
-import { useNavigate } from "react-router-dom";
 import { SignUp } from "@clerk/clerk-react";
 
 const Register = () => {
-  //const navigate = useNavigate();
-
-  //const handleSignUpComplete = () => {
-  //  navigate("/profile-setup"); // Redirect to profile setup
-  //};
 
   return (
     <div className="flex items-center justify-center mt-12">
