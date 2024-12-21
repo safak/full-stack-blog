be4534235608572c2aedfd73a1ab9a4e4882fea6
Sanskruti825 <borhadesanskruti@gmail.com>
2024-12-21 10:38:38 +0530

    Finished routing of search page and route profile

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
 
                 <div className="p-4 relative z-10 w-full text-center flex flex-col items-center justify-content">
                     {/*--------------Add the Search icon---------------------------------------------*/}
-                    <input type="text" placeholder="Seacrh for a swap" className="w-4/5 rounded-xl py-2 px-4 text-black"/>
+                    <input type="text" placeholder="Search for a swap" className="w-4/5 rounded-xl py-2 px-4 text-black"/>
                     <button className="mt-4 px-5 py-2 rounded-2xl text-xl bg-blue-800 text-white-100">Start Swap</button>
                 </div>
             </div>
